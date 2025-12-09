7b9c357506e13fa8bfdb91549448f5839b5dcac3
Ben Njoroge <ben.waithaka.njoroge.ke@gmail.com>
2025-12-09 21:38:00 +0100

    add type command

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 
+COMMANDS = ["exit", "echo", "type"]
 
 def main():
     while True:
@@ -9,6 +10,11 @@ def main():
                 break
             case "echo":
                 sys.stdout.write(f"{" ".join(args)}\n")
+            case "type":
+                if args[0] in COMMANDS:
+                    sys.stdout.write(f"{args[0]} is a shell builtin \n")
+                else:
+                    sys.stdout.write(f"{args[0]}: not found \n")
             case _:
                 sys.stdout.write(f"{command}: command not found")
                 sys.stdout.write("\n")

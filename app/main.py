f86f9251cae2cff8fc6b5360631c228ed25728bc
Ben Njoroge <ben.waithaka.njoroge.ke@gmail.com>
2025-12-09 21:58:37 +0100

    add executable command search in PATH for type command

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
 import sys
+import os
+from pathlib import Path
 
 COMMANDS = ["exit", "echo", "type"]
+
+def handle_type(command: str) -> None:
+    os_bin_paths = os.environ["PATH"].split(os.pathsep)
+
+    if command in COMMANDS:
+        sys.stdout.write(f"{command} is a shell builtin \n")
+        return
+
+    for path in os_bin_paths:
+        if Path(path).exists() and command in os.listdir(path):
+            if os.access(f"{path}/{command}", os.X_OK):
+                sys.stdout.write(f"{command} is {path}/{command} \n")
+                return
+        
+    sys.stdout.write(f"{command}: not found \n")
 
 def main():
     while True:
@@ -11,10 +28,7 @@ def main():
             case "echo":
                 sys.stdout.write(f"{" ".join(args)}\n")
             case "type":
-                if args[0] in COMMANDS:
-                    sys.stdout.write(f"{args[0]} is a shell builtin \n")
-                else:
-                    sys.stdout.write(f"{args[0]}: not found \n")
+                handle_type(args[0])
             case _:
                 sys.stdout.write(f"{command}: command not found")
                 sys.stdout.write("\n")

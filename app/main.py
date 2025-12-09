78dbb3c7cf4a5bdff547ef12462d9dbfb59d1ea3
Ben Njoroge <ben.waithaka.njoroge.ke@gmail.com>
2025-12-09 21:34:29 +0100

    Add echo command

--- app/main.py
+++ app/main.py
@@ -3,11 +3,14 @@ import sys
 
 def main():
     while True:
-        match input("$ "):
+        command, *args = input("$ ").split(" ")
+        match command:
             case "exit":
                 break
-            case invalid_command:
-                sys.stdout.write(f"{invalid_command}: command not found")
+            case "echo":
+                sys.stdout.write(f"{" ".join(args)}\n")
+            case _:
+                sys.stdout.write(f"{command}: command not found")
                 sys.stdout.write("\n")
 
 if __name__ == "__main__":
